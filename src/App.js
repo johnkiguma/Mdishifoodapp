11e10dc8c2bf93665133f351d901f3c1634ded31
johnkiguma <john.kiguma@student.moringaschool.com>
2024-02-15 15:01:10 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -9,16 +9,5 @@ function App(){
     </div>
   )
 }
-import React from "react";
-import NavBar from "./components/NavBar";
-import Footer from "./Footer";
-const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <Footer />
-    </div>
-  );
-};
 
 export default App;
